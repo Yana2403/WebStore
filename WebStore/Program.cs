ffca03acfd2db632cbd64be8d3cdb2affa63db9c
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-05-22 18:10:13 +0400

    Использование конфигурации при настройки приложения

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -13,14 +13,16 @@ namespace WebStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+                .ConfigureWebHostDefaults(host =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    host.UseStartup<Startup>();
                 });
     }
 }
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,7 +13,13 @@ namespace WebStore
 {
     public class Startup
     {
-     
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -26,12 +33,13 @@ namespace WebStore
             }
 
             app.UseRouting();
+            var config_string = Configuration["CustomGreetings"];
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(config_string);
                 });
             });
         }
