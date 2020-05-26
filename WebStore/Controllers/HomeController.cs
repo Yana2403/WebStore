7f9246db23cfbeab25f69e317b11c5b54b2dc783
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-05-26 19:32:43 +0400

    Контроллер для управления сотрудниками

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -11,55 +11,11 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> __Employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                Surname = "Иванов",
-                FirstName = "Иван",
-                Patronymic = "Иванович",
-                Age = 53,
-                Birthday=new DateTime(1967,07,24 ),
-                StartDateofWork=new DateTime(2019,10,01)
-            },
-            new Employee
-            {
-                Id = 2,
-                Surname = "Петров",
-                FirstName = "Пётр",
-                Patronymic = "Петрович",
-                Age = 25,
-                Birthday=new DateTime(1995,03,22),
-               
-                StartDateofWork=new DateTime(2019,12,06)
-            },
-            new Employee
-            {
-                Id = 3,
-                Surname = "Сидоров",
-                FirstName = "Сидор",
-                Patronymic = "Сидорович",
-                Age = 30,
-                Birthday=new DateTime(1990,01,16),
-
-                StartDateofWork=new DateTime(2020,02,22)
-            },
-        };
+        
         public IActionResult Index() => View();
         
-        public IActionResult EmployeeDetails(int id) 
-        {
-            var employee = __Employees.FirstOrDefault(e => e.Id == id); 
-            if (employee is null)
-                return NotFound();//404 ошибка
-
-            return View(employee);
-        }
-        public IActionResult AnotherAction()
-        {
-            return Content("Another action result");
-        }
+        
+  
     }
 
 }
