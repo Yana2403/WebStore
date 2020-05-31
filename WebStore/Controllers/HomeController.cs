cdf75d82bced99a1c998e0dd846bb7d9ac42e9c8
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-05-31 16:18:17 +0400

    Промежуточное ПО

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+        public IActionResult Throw(string id) => throw new ApplicationException(id ?? "Error!"); //генерация исключений
     }
 
 }
