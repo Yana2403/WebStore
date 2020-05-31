499eb98ebdbb562938a94042ed23a2630875f3cf
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-05-31 18:29:05 +0400

    Редактирование даных в контроллере сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -4,25 +4,83 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Data;
+using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.ViewModel;
 
 namespace WebStore.Controllers
 {
     //[Route("NewRoute/[controller]/123")]
     //[Route("Staff")]
     public class EmployeesController : Controller
-    {
-        private static readonly List<Employee> __Employees = TestData.Employees;
+    { private readonly IEmployeesData _EmployeesData;
+        public EmployeesController(IEmployeesData EmployeesData)
+        {
+            _EmployeesData = EmployeesData;
+        }
         //[Route("List")]
-        public IActionResult Index() => View(__Employees);
+        public IActionResult Index() => View(_EmployeesData.Get());
         //[Route("{id}")] 
         public IActionResult EmployeeDetails(int id)
         {
-            var employee = __Employees.FirstOrDefault(e => e.Id == id);
+            var employee = _EmployeesData.GetById(id);
             if (employee is null)
                 return NotFound();//404 ошибка
 
             return View(employee);
         }
+        #region Редактирование
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id is null) return View(new EmployeeViewModel());
+
+            if (Id < 0)
+                return BadRequest();//ошибка 400
+
+            var employee = _EmployeesData.GetById((int)Id);
+            if (employee is null)
+                return NotFound(); //404 ошибка
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Surname = employee.Surname,
+                Name = employee.FirstName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age,
+                Birthday = employee.Birthday,
+                StartDateofWork = employee.StartDateofWork 
+             });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel Model)
+        {
+            if (Model is null)
+                throw new ArgumentNullException(nameof(Model));
+
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                FirstName = Model.Name,
+                Surname = Model.Surname,
+                Patronymic = Model.Patronymic,
+                Age = Model.Age,
+                Birthday = Model.Birthday,
+                StartDateofWork = Model.StartDateofWork
+            };
+
+            if (Model.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Edit(employee);
+
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
     }
 }
