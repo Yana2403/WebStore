4d8051c469fb5fe12a867a2187a58ebdb627da9c
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-06-03 16:21:04 +0400

    Определение товара и его фильтра

---
+++ WebStore.Domain/Entities/ProductFilter.cs
@@ -0,0 +1,9 @@
+﻿namespace WebStore.Domain.Entities
+{
+    public class ProductFilter //для возможности фильтрации товаров
+    {
+        public int? SectionId { get; set; }
+
+        public int? BrandId { get; set; }
+    }
+}
