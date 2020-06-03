9400a40ea07080ebe127655f64dead7045e738c6
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-06-03 16:26:05 +0400

    Извлечение каталога товаров с фильтром

--- WebStore/Infrastructure/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -11,5 +11,6 @@ namespace WebStore.Infrastructure
         IEnumerable<Section> GetSections();
 
         IEnumerable<Brand> GetBrands();
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null); //возвращает перечисление товаров
     }
 }
