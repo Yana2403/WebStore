41f6db0740290fb88704549f739d42a7b12c7c8f
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-06-03 15:32:08 +0400

    Сервис каталога товаров в памяти

---
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryProductData:IProductData
+    {
+        public IEnumerable<Section> GetSections() => TestData.Sections;
+
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;
+    }
+}
