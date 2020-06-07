16e5935edd832b8201bc6cb067f80e633737af7c
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-06-07 15:31:12 +0400

    Доработка моделей для передачи в бд

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Table("ProductBrand")]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
--- WebStore.Domain/Entities/Product.cs
+++ WebStore.Domain/Entities/Product.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
-{
+{   //[Table("Products")]
     public class Product : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
 
         public int SectionId { get; set; }
 
+        [ForeignKey(nameof(SectionId))]
+        public virtual Section Section { get; set; } //навигационное св-во
+
         public int? BrandId { get; set; }
 
+        [ForeignKey(nameof(Brand))]
+        public virtual Brand Brand { get; set; }
+
+        [Required]
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
