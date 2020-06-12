d7c4d9aae24e45c2c72d5acc3a6d19c644fc3068
Яна Улендеева <ulendeeva.yana2013@yandex.ru>
2020-06-12 23:39:29 +0400

    Подготовка представления и контроллера для регистрации

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModel.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        #region Register new user
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+        public IActionResult Register(RegisterUserViewModel Model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        #endregion
+
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => View();
+
+        public IActionResult AccessDenied() => View();
+    }
+}
